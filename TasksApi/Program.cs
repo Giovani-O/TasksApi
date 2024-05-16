57dd393d3aab7af289cc909d4aee1bb09a3e887e
Giovani de Oliveira <giovani98@proton.me>
2024-05-16 14:16:04 -0300

    feat: Create put and delete endpoints

--- TasksApi/Program.cs
+++ TasksApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,11 +24,57 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/tasks", async (AppDbContext db) => await db.Tasks.ToListAsync());
 
+app.MapGet("/tasks/{id}", async (int id, AppDbContext db) =>
+    await db.Tasks.FindAsync(id) is Task task 
+        ? Results.Ok(task) 
+        : Results.NotFound()
+);
+
+app.MapGet("/tasks/finished", async (AppDbContext db) => 
+    await db.Tasks.Where(x => x.IsFinished).ToListAsync()
+);
+
 app.MapPost("/tasks", async (Task task, AppDbContext db) =>
 {
     db.Tasks.Add(task);
     await db.SaveChangesAsync();
     return Results.Created($"/tasks/{task.Id}", task);
+});
+
+app.MapPost("/tasks/add-multiple", async (List<Task> tasks, AppDbContext db) =>
+{
+    foreach (var task in tasks)
+    {
+        db.Tasks.Add(task);
+    }
+    await db.SaveChangesAsync();
+    return Results.Created($"/tasks", tasks);
+});
+
+app.MapPut("/tasks/{id}", async (int id, Task updatedTask, AppDbContext db) => 
+{ 
+    var task = await db.Tasks.FindAsync(id);
+
+    if (task is null)
+        return Results.NotFound();
+
+    task.Name = updatedTask.Name;
+    task.IsFinished = updatedTask.IsFinished;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/tasks/{id}", async(int id, AppDbContext db) =>
+{
+    var taskToDelete = await db.Tasks.FindAsync(id);
+
+    if (taskToDelete is not Task task)
+        return Results.NotFound();
+
+    db.Tasks.Remove(taskToDelete);
+    await db.SaveChangesAsync();
+    return Results.Ok(taskToDelete);
 });
 
 app.Run();
